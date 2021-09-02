45051618879d3f6aa1afb4935932ec9a7fa1d1b6
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-09-02 11:10:57 +0800

    update

--- SupermanSaves.m
+++ SupermanSaves.m
@@ -16,7 +16,7 @@ classdef SupermanSaves
         end
         function self = getSupermanVo(self)
             sec_p = sqrt((2*self.height)/self.g) - self.dt;
-            self.SM_Vo = (-(self.height))+(1/2*self.g*sec_p);
+            self.SM_Vo = -((-(self.height) + (1/2)*self.g*(sec_p).^2))/-sec_p;
         end
         function self = getMinHeight(self)
             self.min_height = 1/2*self.g*power(self.dt, 2);
@@ -24,13 +24,15 @@ classdef SupermanSaves
         function self = plotGraph(self)
             x = [0:0.1:10];
             st_func = 180-((1/2)*self.g*x.^2);
-            sm_func = 180-(174.4848481*(x-self.dt)+(1/2)*self.g*(x-self.dt).^2);
+            sm_func = 180-(-self.SM_Vo*(x-self.dt)+(1/2)*self.g*(x-self.dt).^2);
             plot(x,st_func,x,sm_func);
+            title('POISITIONS OF STUDENT AND SUPERMAN AS FUNCTIONS OF TIME');
             xlim([0,10]);
             ylim([0,190]);
             xlabel('Time (s)');
             ylabel('Height (m)');
             legend({'Student', 'Superman'});
+            grid on;
         end
     end
 end
